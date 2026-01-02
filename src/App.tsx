73d01352a14984b54c234978baa1da08ae37a62c
Marlon P. Dias <marlondias@users.noreply.github.com>
2026-01-02 09:16:08 -0300

    feat: Added placeholder for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,39 @@
+import { useState } from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import Home from './pages/Home'
+import Trending from './pages/Trending'
+import UserProfile from './pages/UserProfile'
+import HashtagDetails from './pages/HashtagDetails'
 
 export default function App() {
-  const isAuthenticated = true //mock
+  const [isAuthenticated, setIsAuthenticated] = useState(true)
 
   return (
     <>
+      <div>
+        <button onClick={() => setIsAuthenticated(!isAuthenticated)}>
+          Toggle Auth: {isAuthenticated.toString()}
+        </button>
+      </div>
       <BrowserRouter>
         <Routes>
-          {isAuthenticated && (
+          {isAuthenticated ? (
             <>
-              <Route path="/home" element={<div>Home</div>} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/trending" element={<Trending />} />
+              <Route path="/u/:username" element={<UserProfile />} />
+              <Route path="/h/:hashtag" element={<HashtagDetails />} />
+              <Route path="*" element={<Navigate to="/home" replace />} />
+            </>
+          ) : (
+            <>
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="*" element={<Navigate to="/login" replace />} />
             </>
           )}
-
-          <Route
-            path="/login"
-            element={
-              isAuthenticated ? <Navigate to="/home" replace /> : <div>Login</div>
-            }
-          />
-
-          <Route
-            path="*"
-            element={
-              <Navigate to={isAuthenticated ? '/home' : '/login'} replace />
-            }
-          />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/HashtagDetails.tsx
@@ -0,0 +1,3 @@
+export default function HashtagDetails() {
+    return <div>HashtagDetails</div>
+}
