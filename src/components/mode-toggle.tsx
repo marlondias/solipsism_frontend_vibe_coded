d01e48aa8163b827e7f6f3d6285c734dd22b349f
Marlon P. Dias <marlondias@users.noreply.github.com>
2026-01-02 19:35:04 -0300

    fix: Problem with overlaping icons on color toggle

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -15,11 +15,9 @@ export function ModeToggle() {
 
     return (
         <Button variant="outline" size="icon" onClick={cycleTheme} title={`Current theme: ${theme}`}>
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            {theme === "system" && (
-                <Monitor className="absolute h-[1.2rem] w-[1.2rem] opacity-50" />
-            )}
+            {theme === "light" && <Sun className="h-[1.2rem] w-[1.2rem]" />}
+            {theme === "dark" && <Moon className="h-[1.2rem] w-[1.2rem]" />}
+            {theme === "system" && <Monitor className="h-[1.2rem] w-[1.2rem]" />}
             <span className="sr-only">Toggle theme</span>
         </Button>
     )
